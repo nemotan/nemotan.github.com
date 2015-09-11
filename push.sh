c06175567e2160c44b045894c766a353d4dd6ae9
nemotan <nemotan@126.com>
2015-09-11 15:56:08 +0800

    修复分类视图下排列不整齐的样式问题

--- push.sh
+++ push.sh
@@ -4,7 +4,13 @@ sh hide.sh
 echo "替换成功!"
 echo "同步中......"
 git add *
-git commit -m "同步blog"
+if [ -n $1 ];
+  then
+    git commit -m "$1" 
+else 
+    git commit -m "同步blog"
+fi
+
 git status
 git push
 echo "同步完毕！"
